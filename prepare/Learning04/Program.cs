ca7d9f345ee9616fd5fad365104e0f65e8ade63b
David Labra Gaona <davidlabragaona@gmail.com>
2024-02-02 20:20:22 -0300

    inheritance learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("David Labra Gaona", "Algebra");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("David Labra Gaona", "Programming", "7.3", "8-19");
+        Console.WriteLine($"{mathAssignment.GetSummary()}\n{mathAssignment.GetHomeworkList()}");
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("David Labra Gaona", "European History", "The Causes of World War II");
+        Console.WriteLine($"{writingAssignment.GetSummary()}\n{writingAssignment.GetWritingInformation()}");
     }
 }
