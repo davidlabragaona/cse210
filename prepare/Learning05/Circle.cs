cfd190d97e3a6d2cc345beca25c7de9f00e1a214
David Labra Gaona <davidlabragaona@gmail.com>
2024-02-11 20:06:41 -0300

    polymorphism learning activity

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,20 @@
+public class Circle : Shape
+{
+    private double _radius;
+
+    public void SetRadius(double radius) {
+        _radius = radius;
+    }
+
+    public double GetRadius() {
+        return _radius;
+    }
+
+    public Circle (string color, double radius) : base(color) {
+        SetRadius(radius);
+    }
+
+    public override double GetArea() {
+        return Math.PI * Math.Pow(GetRadius(), 2);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Square square = new Square("red", 20.00);
+        //Console.WriteLine ($"{square.GetColor()}-{square.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 20));
+        shapes.Add(new Rectangle("Green", 20, 30));
+        shapes.Add(new Circle("Blue", 15));
+
+        foreach (Shape shape in shapes) {
+            Console.WriteLine($"{shape.GetColor()} - {shape.GetArea()}");
+        }
     }
 }
