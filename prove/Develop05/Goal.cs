a706a0882afac4b6b9c86284e03bb3f414ef97d8
David Labra Gaona <davidlabragaona@gmail.com>
2024-02-11 23:49:30 -0300

    partial w05 development

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,13 @@
+public class Goal
+{
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal (string name, string description, string points) {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+}
